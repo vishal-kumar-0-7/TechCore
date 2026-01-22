cb6bf06f5c92b30e0530ce3f1055623c2d10692f
AYUSHBVP <ayushkumarthakur2210@gmail.com>
2026-01-22 15:46:35 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/services/Home';
 import WebDevelopment from './pages/services/WebDevelopment';
 import AppDevelopment from './pages/services/AppDevelopment';
+import AiAutomation from './pages/services/ai_automation';
+
 
 import StartupSolution from './pages/solutions/Startup';
 function App() {
@@ -16,8 +18,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/services/web-development" element={<WebDevelopment />} />
         <Route path="/services/app-development" element={<AppDevelopment />} />
-
-
+        <Route path="/services/ai-automation" element={<AiAutomation />} />
         
         <Route path="/solutions/startup" element={<StartupSolution/>}/>
         
--- src/pages/services/ai_automation.jsx
+++ src/pages/services/ai_automation.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const AiAutomation = () => (
   <main className="max-w-7xl mx-auto p-6">
-    <h1 className="text-3xl font-bold">App Development</h1>
+    <h1 className="text-3xl font-bold">AI Automation</h1>
     <p className="mt-4">Page content goes here.</p>
   </main>
 );
 
-export default AppDevelopment;
+export default AiAutomation;
